a6d944a3f5c0139c6421954053b2cadf3568c390
Tangjj1996 <Tangjj_1996@163.com>
2020-04-11 13:52:57 +0800

    docs: title & second title

---
+++ README.md
@@ -0,0 +1,4 @@
+<center>
+	<font color='cyan' size='6'>smart-paint</font>
+<center>
+<center>awesome instant painting application running in any platform</center>
--- scripts/commit-lint.js
+++ scripts/commit-lint.js
@@ -5,7 +5,6 @@ const msg = require('fs').readFileSync(msgPath, 'utf-8').trim();
 const commitRE = /^(v\d+\.\d+\.\d+(-(alpha|beta|rc.\d+))?)|((revert: )?(feat|fix|docs|style|refactor|perf|test|workflow|ci|chore|types|update)(\(.+\))?!?: (:.{1,30}:)?.{1,50})/;
 
 if (!commitRE.test(msg)) {
-  console.log();
   console.error(
     `  ${chalk.bgRed.white(' ERROR ')} ${chalk.red('invalid commit message format.')}\n\n${
       chalk.red('  Proper commit message format is required for automated changelog generation. Examples:\n\n')
