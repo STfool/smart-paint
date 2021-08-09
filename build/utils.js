61f172fec0ac8deddb6991876ac61fff31dacae1
Tangjj1996 <tangjj_1996@163.com>
2021-08-09 10:43:05 +0800

    feat: electron && webpack config

---
+++ build/utils.js
@@ -0,0 +1,43 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getLoadUrl = void 0;
+const url = __importStar(require("url"));
+const path = __importStar(require("path"));
+const config_json_1 = __importDefault(require("./config.json"));
+process.env.NODE_ENV = config_json_1.default.env;
+const getLoadUrl = () => {
+    if (process.env.NODE_ENV === "development") {
+        return "http://localhost:8080";
+    }
+    else if (process.env.NODE_ENV === "production") {
+        return url.format({
+            pathname: path.join(__dirname, "index.html"),
+            protocol: "file:",
+            slashes: true,
+        });
+    }
+    throw new Error("[utils] get environment error");
+};
+exports.getLoadUrl = getLoadUrl;
