749e089f6ad0ef31b6e1954262c54463ff28a3da
Tangjj1996 <tangjj_1996@163.com>
2021-08-09 23:42:38 +0800

    feat: add canvas paint feature

--- page/app.tsx
+++ page/app.tsx
@@ -1,16 +1,36 @@
 import * as React from "react";
 
 const App: React.FC = () => {
+    const [resolution] = React.useState(window.devicePixelRatio);
+
+    React.useEffect(() => {
+        const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+        const ctx = canvas.getContext("2d");
+
+        ctx.beginPath();
+        ctx.moveTo(0, 100);
+        ctx.lineTo(100, 100);
+        ctx.closePath();
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(20, 30);
+        ctx.lineTo(20, 100);
+        ctx.closePath();
+        ctx.stroke();
+    }, []);
     return (
-        <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
-            <div className="flex-shrink-0">
-                <img src="" alt="ChitChat Logo" className="h-12 w-12" />
-            </div>
-            <div>
-                <div className="text-xl font-medium text-black">ChitChat</div>
-                <p className="text-gray-500">You are a new message</p>
-            </div>
-        </div>
+        <>
+            <canvas
+                id="canvas"
+                width={window.innerWidth * resolution}
+                height={window.innerHeight * resolution}
+                style={{
+                    width: window.innerWidth + "px",
+                    height: window.innerHeight + "px",
+                }}
+            ></canvas>
+        </>
     );
 };
 
