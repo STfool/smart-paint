0800d7bd87edd96ea2c6638868bcd67503cf8097
Tangjj1996 <tangjj_1996@163.com>
2021-08-13 09:39:54 +0800

    feat: graphic canvas line

--- page/app.tsx
+++ page/app.tsx
@@ -2,38 +2,42 @@ import React, { useEffect, useRef, useState } from "react";
 import useBoard from "./hooks/useBoard";
 
 const App: React.FC = () => {
-    const [resolution] = useState(window.devicePixelRatio);
+    const resolutionRef = useRef(window.devicePixelRatio);
     const pointerDownRef = useRef(false);
-    const [position, setPosition] = useState({ x: 0, y: 0 });
-    const [width, height] = useBoard();
     const canvasRef = useRef<HTMLCanvasElement>();
 
+    const [width, height] = useBoard();
     useEffect(() => {
         const ctx = canvasRef.current.getContext("2d");
     }, [width, height]);
 
+    const positionRef = useRef({ x: 0, y: 0 });
     useEffect(() => {
         const ctx = canvasRef.current.getContext("2d");
         const handlePointerDown = (e: PointerEvent) => {
             if (!pointerDownRef.current) {
                 pointerDownRef.current = true;
             }
-            setPosition({
+            positionRef.current = {
                 x: e.pageX,
                 y: e.pageY,
-            });
+            };
         };
         const handlePointerMove = (e: PointerEvent) => {
             if (!pointerDownRef.current) return;
 
             ctx.beginPath();
-            ctx.moveTo(position.x * resolution, position.y * resolution);
-            ctx.lineTo(e.pageX * resolution, e.pageY * resolution);
+            ctx.moveTo(positionRef.current.x * resolutionRef.current, positionRef.current.y * resolutionRef.current);
+            ctx.lineTo(e.pageX * resolutionRef.current, e.pageY * resolutionRef.current);
             ctx.closePath();
             ctx.stroke();
+
+            positionRef.current = {
+                x: e.pageX,
+                y: e.pageY,
+            };
         };
         const handlePointerUp = () => {
-            console.log(pointerDownRef.current);
             pointerDownRef.current = false;
         };
         window.addEventListener("pointerdown", handlePointerDown, false);
@@ -51,8 +55,8 @@ const App: React.FC = () => {
         <>
             <canvas
                 ref={canvasRef}
-                width={width * resolution}
-                height={height * resolution}
+                width={width * resolutionRef.current}
+                height={height * resolutionRef.current}
                 style={{ width: width + "px", height: height + "px", cursor: "crosshair" }}
             ></canvas>
         </>
